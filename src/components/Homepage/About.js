d6816a5cd5c705b2e69a51641d767083d18112c2
devalibello <alibelloimoukhuede@gmail.com>
2024-05-08 23:24:16 +0100

    Add about content

--- src/components/Homepage/About.js
+++ src/components/Homepage/About.js
@@ -5,6 +5,12 @@ const About = () => (
     <section className="about-section">
         <div className="about-picture-section">
             <img src={aboutPicture} alt="" className="about-picture" />
+            <ul className="about-content">
+                <li className="about-content-title">Come and visit us</li>
+                <li className="about-content-phone">234</li>
+                <li className="about-content-mail">alibelloimoukhuede@gmail</li>
+                <li className="about-content-location">Yankaba</li>
+            </ul>
         </div>
         <ul className="about-details-section">
             <li className="about-title">We provide healthy <br />food for your family</li>
