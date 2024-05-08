a622dbc42cb00c7e9d04739145f3562cf5bb3be5
devalibello <alibelloimoukhuede@gmail.com>
2024-05-08 20:20:08 +0100

    Update menu cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Homepage/Navbar'
 import Banner from './components/Homepage/Banner'
+import Menu from './components/Homepage/Menu'
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Banner />
+      <Menu />
     </>
   );
 }
--- src/components/Homepage/Menu.js
+++ src/components/Homepage/Menu.js
@@ -1,22 +1,48 @@
-
+import '../../styles/Menu.css'
+import breakfastIcon from '../../assets/breakfast-icon.png'
 
 
 const Menu = () => {
     
-    const menuList = [
-        {
-            id: 1,
-            icon: breakfastIcon,
-            title: 'Breakfast',
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit atque aperiam, amet ex voluptate."
-        },
-    ]
+    // const menuList = [
+    //     {
+    //         id: 1,
+    //         icon: breakfastIcon,
+    //         title: 'Breakfast',
+    //         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit atque aperiam, amet ex voluptate."
+    //     },
+    //     {
+    //         id: 2,
+    //         icon: mainDishesIcon,
+    //         title: 'Main Dishes',
+    //         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit atque aperiam, amet ex voluptate."
+    //     },
+    //     {
+    //         id: 3,
+    //         icon: drinksIcon,
+    //         title: 'Drinks',
+    //         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit atque aperiam, amet ex voluptate."
+    //     },
+    //     {
+    //         id: 4,
+    //         icon: dessertsIcon,
+    //         title: 'Desserts',
+    //         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit atque aperiam, amet ex voluptate."
+    //     },
+    // ]
     
     
     return (
-    <section className="menu-section">
-    <div className="menu-title">Browse Our Menu</div>
-
+    <section className="menu-card-section">
+        <div className="menu-title">Browse Our Menu</div>
+        <div className="menu-card-container">
+            <ul className="menu-cards">
+                <li className="icon"><img src={breakfastIcon} alt="" className="menu-icon" /></li>
+                <li className="card-title">Breakfast</li>
+                <li className="card-descriotion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, minus ex rerum voluptates</li>
+                <li className="card-cta">Explor Menu</li>
+            </ul>
+        </div>
     </section>
 
 )};
