3d9ece0ab6f04e601d9aeefde2a5d9ff113ce008
devalibello <alibelloimoukhuede@gmail.com>
2024-05-08 19:16:16 +0100

    Add Menu section

---
+++ src/components/Homepage/Menu.js
@@ -0,0 +1,24 @@
+
+
+
+const Menu = () => {
+    
+    const menuList = [
+        {
+            id: 1,
+            icon: breakfastIcon,
+            title: 'Breakfast',
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit atque aperiam, amet ex voluptate."
+        },
+    ]
+    
+    
+    return (
+    <section className="menu-section">
+    <div className="menu-title">Browse Our Menu</div>
+
+    </section>
+
+)};
+
+export default Menu;
