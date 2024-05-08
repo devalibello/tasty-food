5291857977ca423d2188016778f98d755a19aa87
devalibello <alibelloimoukhuede@gmail.com>
2024-05-08 22:54:15 +0100

    Add about picture

--- src/components/Homepage/About.js
+++ src/components/Homepage/About.js
@@ -6,8 +6,12 @@ const About = () => (
         <div className="about-picture-section">
             <img src={aboutPicture} alt="" className="about-picture" />
         </div>
-        <div className="about-details-section">
-        </div>
+        <ul className="about-details-section">
+            <li className="about-title">We provide healthy <br />food for your family</li>
+            <li className="about-description-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit eos veniam aliquid autem sequi voluptatibus expedita impedit omnis quisquam doloribus.</li>
+            <li className="about-description-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure, corrupti aliquid harum expedita unde autem.</li>
+            <li className="more-about-us">More About Us</li>
+        </ul>
     </section>
 
 );
