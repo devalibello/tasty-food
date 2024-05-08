5fa71cc5cb403a7613672a5923331334871b2646
devalibello <alibelloimoukhuede@gmail.com>
2024-05-08 14:30:18 +0100

    Add Nav structure

---
+++ src/components/Homepage/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Navbar = () => (
+<section className="nav-section">
+    <div className="contact-section">
+        <div className="contact-list">
+
+        </div>
+        <div className="social-list">
+
+        </div>
+    </div>
+    <div className="menu-section">
+        <div className="logo-section"></div>
+        <div className="menu-list"></div>
+        <div className="book-a-table"></div>
+    </div>
+</section>
+);
+
+export default Navbar;
