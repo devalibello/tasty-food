2ca9cc8692a26a965168891e136eb550661cc28a
devalibello <alibelloimoukhuede@gmail.com>
2024-05-12 00:33:05 +0100

    Update about section

--- src/components/Homepage/About.js
+++ src/components/Homepage/About.js
@@ -1,5 +1,8 @@
 import aboutPicture from '../../assets/about.png'
 import '../../styles/About.css'
+import { faEnvelope,faPhone,faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 const About = () => (
     <section className="about-section">
@@ -7,9 +10,9 @@ const About = () => (
             <img src={aboutPicture} alt="" className="about-picture" />
             <ul className="about-content">
                 <li className="about-content-title">Come and visit us</li>
-                <li className="about-content-phone">234</li>
-                <li className="about-content-mail">alibelloimoukhuede@gmail</li>
-                <li className="about-content-location">Yankaba</li>
+                <li className="about-content-phone about-contact"><span className='about-icon'><FontAwesomeIcon icon={faPhone} /></span>+234 706 6100 601</li>
+                <li className="about-content-mail about-contact"><span className='about-icon'><FontAwesomeIcon icon={faEnvelope} /></span>alibelloimoukhuede@gmail</li>
+                <li className="about-content-location about-contact"><span className='about-icon'><FontAwesomeIcon icon={faLocationDot} /></span>Yankaba Quarters</li>
             </ul>
         </div>
         <ul className="about-details-section">
