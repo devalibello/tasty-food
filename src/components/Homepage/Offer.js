981a842bc268fa47988e60fb703b6b58f7081698
devalibello <alibelloimoukhuede@gmail.com>
2024-05-12 14:51:13 +0100

    Add offer dynamically

--- src/components/Homepage/Offer.js
+++ src/components/Homepage/Offer.js
@@ -1,37 +1,45 @@
 import '../../styles/Offer.css'
 import offerItemOne from '../../assets/offer-item-1.png'
 
-// const offerItems = [
-//     {
-
-//     }
-// ]
+const offerItems = [
+    {
+        id: 1,
+        image: cateringsImage,
+        title: 'Caterings',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, facilis."
+    },
+    {
+        id: 2,
+        image: birthdaysImage,
+        title: 'Birthdays',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, facilis."
+    },
+    {
+        id: 3,
+        image: weddingsImage,
+        title: 'Weddings',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, facilis."
+    },
+    {
+        id: 4,
+        image: eventsImage,
+        title: 'Events',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, facilis."
+    }
+]
 
 
 const Offer = () => (
 <section className="offer-section">
     <div className="offer-heading">We also offer unique <br />services for your events</div>
     <div className="offer-items-container">
+    { offerItems.map((offer) => 
         <div className="offer-item-list">
-            <img src={offerItemOne} alt="" className="offer-item-image" />
-            <div className="offer-item-header">Catering</div>
-            <div className="offer-item-details">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+            <img src={offer.image} alt="" className="offer-item-image" />
+            <div className="offer-item-header">{offer.title}</div>
+            <div className="offer-item-details">{offer.description}</div>
         </div>
-        <div className="offer-item-list">
-            <img src={offerItemOne} alt="" className="offer-item-image" />
-            <div className="offer-item-header">Catering</div>
-            <div className="offer-item-details">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-        </div>
-        <div className="offer-item-list">
-            <img src={offerItemOne} alt="" className="offer-item-image" />
-            <div className="offer-item-header">Catering</div>
-            <div className="offer-item-details">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-        </div>
-        <div className="offer-item-list">
-            <img src={offerItemOne} alt="" className="offer-item-image" />
-            <div className="offer-item-header">Catering</div>
-            <div className="offer-item-details">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-        </div>
+    )}
     </div>
 </section>
 );
