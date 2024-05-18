e0f1819589b1f1ef1f64bb02a281956822809cc8
devalibello <alibelloimoukhuede@gmail.com>
2024-05-18 23:58:40 +0100

    Add delivery sections

--- src/components/Homepage/Delivery.js
+++ src/components/Homepage/Delivery.js
@@ -2,8 +2,8 @@ import '../../styles/Delivery.css'
 
 const Delivery = () => (
     <section className="delivery-section">
-        
+
     </section>
 );
 
-export default Delivery;
+export default Delivery
