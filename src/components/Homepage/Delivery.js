ea29c4a6dde114a6bc21c2ec4df8495b360289ca
devalibello <alibelloimoukhuede@gmail.com>
2024-05-19 23:16:36 +0100

    Add delivery description text

--- src/components/Homepage/Delivery.js
+++ src/components/Homepage/Delivery.js
@@ -6,6 +6,8 @@ const Delivery = () => (
         <div className="picture-middle"></div>
         <div className="delivery-section-text">
             <div className="delivery-title">Fast food delivery in city</div>
+            <div className="delivery-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, voluptate.</div>
+            
         </div>
     </section>
 );
