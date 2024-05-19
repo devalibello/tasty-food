f985a85fbfe2d55fd7191ea15e4289e87dc346ec
devalibello <alibelloimoukhuede@gmail.com>
2024-05-19 23:15:30 +0100

    Add delivery title

--- src/components/Homepage/Delivery.js
+++ src/components/Homepage/Delivery.js
@@ -4,7 +4,9 @@ const Delivery = () => (
     <section className="delivery-section">
         <div className="picture-left"></div>
         <div className="picture-middle"></div>
-        <div className="delivery-section-text"></div>
+        <div className="delivery-section-text">
+            <div className="delivery-title"></div>
+        </div>
     </section>
 );
 
