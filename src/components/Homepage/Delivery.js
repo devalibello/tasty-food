3e863dc636769ba58357722f1da6170127a535fc
devalibello <alibelloimoukhuede@gmail.com>
2024-05-19 23:15:57 +0100

    Add delivery title text

--- src/components/Homepage/Delivery.js
+++ src/components/Homepage/Delivery.js
@@ -5,7 +5,7 @@ const Delivery = () => (
         <div className="picture-left"></div>
         <div className="picture-middle"></div>
         <div className="delivery-section-text">
-            <div className="delivery-title"></div>
+            <div className="delivery-title">Fast food delivery in city</div>
         </div>
     </section>
 );
