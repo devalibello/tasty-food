210858ed3ec0e1232fcdfaf0de7ae87d735ac745
devalibello <alibelloimoukhuede@gmail.com>
2024-05-19 23:14:35 +0100

    Add delivery structure

--- src/components/Homepage/Delivery.js
+++ src/components/Homepage/Delivery.js
@@ -2,7 +2,9 @@ import '../../styles/Delivery.css'
 
 const Delivery = () => (
     <section className="delivery-section">
-
+        <div className="picture-left"></div>
+        <div className="picture-middle"></div>
+        <div className="delivery-section-text"></div>
     </section>
 );
 
