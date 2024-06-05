dd4abf3325d30fa59033a57d04a39f99a3c5fbb4
devalibello <alibelloimoukhuede@gmail.com>
2024-06-05 10:59:56 +0100

    Update customers section

--- src/components/Homepage/Customers.js
+++ src/components/Homepage/Customers.js
@@ -1,3 +1,32 @@
+import '../../styles/Customers'
+
+const customers = [
+    {
+        id: 1,
+        image: cateringsImage,
+        title: 'Caterings',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, facilis.",
+        name: "Sophie Robbert",
+        location: "Los Angeles, CA"
+    },
+    {
+        id: 1,
+        image: cateringsImage,
+        title: 'Caterings',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, facilis.",
+        name: "Sophie Robbert",
+        location: "Los Angeles, CA"
+    },
+    {
+        id: 1,
+        image: cateringsImage,
+        title: 'Caterings',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, facilis.",
+        name: "Sophie Robbert",
+        location: "Los Angeles, CA"
+    }
+]
+
 const Customers = () => (
     <section className="customers-section">
     </section>
