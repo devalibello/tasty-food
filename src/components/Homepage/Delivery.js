cc39a0994e5880240967dc92b793cf9859cef350
devalibello <alibelloimoukhuede@gmail.com>
2024-06-05 10:48:32 +0100

    Add complete delivery section

--- src/components/Homepage/Delivery.js
+++ src/components/Homepage/Delivery.js
@@ -2,6 +2,9 @@ import '../../styles/Delivery.css'
 import deliveryImageLeft from '../../assets/delivery_1.png'
 import deliveryImageUp from '../../assets/delivery_2.png'
 import deliveryImageDown from '../../assets/delivery_3.png'
+import deliveryIconOne from '../../assets/delivery-icon-1.png'
+import deliveryIconTwo from '../../assets/delivery-icon-2.png'
+import deliveryIconThree from '../../assets/delivery-icon-3.png'
 
 const Delivery = () => (
     <section className="delivery-section">
@@ -15,11 +18,20 @@ const Delivery = () => (
         <div className="delivery-section-text">
             <div className="delivery-title">Fast Food <br /> Delivery in city</div>
             <div className="delivery-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, voluptate.</div>
-            <ul>
-                <li>Delivery within 30 mins</li>
-                <li>Best Offer & Prices</li>
-                <li>Online Service available</li>
-            </ul>
+            <div className='delivery-icon-section'>
+                <div className='delivery-icon'>
+                    <img src={deliveryIconOne} alt="" className="delivery-icon-1" />
+                    <div>Delivery within 30 mins</div>
+                </div>
+                <div className='delivery-icon'>
+                    <img src={deliveryIconTwo} alt="" className="delivery-icon-2" />
+                    <div>Best Offer & Prices</div>
+                </div>
+                <div className='delivery-icon'>
+                    <img src={deliveryIconThree} alt="" className="delivery-icon-3" />
+                    <div>Online Service available</div>
+                </div>
+            </div>
         </div>
     </section>
 );
