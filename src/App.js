310282e08c5d7d8be26a70a373f05c3b5a1e4d49
devalibello <alibelloimoukhuede@gmail.com>
2024-06-05 08:54:10 +0100

    Add delivery pics

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Banner from './components/Homepage/Banner'
 import Menu from './components/Homepage/Menu'
 import About from './components/Homepage/About'
 import Offer from './components/Homepage/Offer'
+import Delivery from './components/Homepage/Delivery'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Menu />
       <About />
       <Offer />
+      <Delivery />
     </>
   );
 }
--- src/components/Homepage/Delivery.js
+++ src/components/Homepage/Delivery.js
@@ -1,9 +1,16 @@
 import '../../styles/Delivery.css'
+import deliveryImageLeft from '../../assets/delivery_1.png'
+import deliveryImageUp from '../../assets/delivery_2.png'
+import deliveryImageDown from '../../assets/delivery_3.png'
 
 const Delivery = () => (
     <section className="delivery-section">
         <div className="picture-left"></div>
-        <div className="picture-middle"></div>
+            <img src={deliveryImageLeft} alt="" className="delivery-image-left" />
+        <div className="picture-middle">
+            <img src={deliveryImageUp} alt="" className="delivery-image-up" />
+            <img src={deliveryImageDown} alt="" className="delivery-image-down" />
+        </div>
         <div className="delivery-section-text">
             <div className="delivery-title">Fast food delivery in city</div>
             <div className="delivery-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, voluptate.</div>
