2fafd30c99ac7055b9087ba13dd5073e0dda303c
devalibello <alibelloimoukhuede@gmail.com>
2024-06-06 17:14:48 +0100

    Add blog list

--- src/components/Homepage/Blog.js
+++ src/components/Homepage/Blog.js
@@ -3,8 +3,10 @@ import '../../styles/Blog.css'
 const blogDetails = [
     {
         id: 1,
-        
-
+        image: blogImage1,
+        date: "May 3, 2024",
+        title: Lorem ipsum dolor sit amet consectetur adipisicing elit.,
+        description: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste veritatis facilis, est totam maiores nihil eligendi doloremque aliquam possimus dolorum,
     },
 ]
 
