6b297257ab131c490648b940c13cf68acaf309e9
devalibello <alibelloimoukhuede@gmail.com>
2024-07-31 22:59:47 +0100

    Add animation to menu component

--- src/components/Homepage/Menu.js
+++ src/components/Homepage/Menu.js
@@ -1,44 +1,55 @@
 import '../../styles/Menu.css'
+import { useRef, useEffect } from 'react';
+import gsap from 'gsap';
 import breakfastIcon from '../../assets/breakfast-icon.webp';
 import drinksIcon from '../../assets/drinks-icon.webp';
 import mainDishesIcon from '../../assets/maindish-icon.webp';
 import dessertsIcon from '../../assets/desserts-icon.webp';
 
+const menuList = [
+    {
+        id: 1,
+        icon: breakfastIcon,
+        title: 'Breakfast',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit atque aperiam, amet ex voluptate."
+    },
+    {
+        id: 2,
+        icon: mainDishesIcon,
+        title: 'Main Dishes',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit atque aperiam, amet ex voluptate."
+    },
+    {
+        id: 3,
+        icon: drinksIcon,
+        title: 'Drinks',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit atque aperiam, amet ex voluptate."
+    },
+    {
+        id: 4,
+        icon: dessertsIcon,
+        title: 'Desserts',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit atque aperiam, amet ex voluptate."
+    },
+]
 
 const Menu = () => {
-    
-    const menuList = [
-        {
-            id: 1,
-            icon: breakfastIcon,
-            title: 'Breakfast',
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit atque aperiam, amet ex voluptate."
-        },
-        {
-            id: 2,
-            icon: mainDishesIcon,
-            title: 'Main Dishes',
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit atque aperiam, amet ex voluptate."
-        },
-        {
-            id: 3,
-            icon: drinksIcon,
-            title: 'Drinks',
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit atque aperiam, amet ex voluptate."
-        },
-        {
-            id: 4,
-            icon: dessertsIcon,
-            title: 'Desserts',
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit atque aperiam, amet ex voluptate."
-        },
-    ]
-    
+
+    const menuRef = useRef(null);
+
+    const animateMenu = () => {
+        const menuAnimate = menuRef.current.querySelectorAll('div');
+        gsap.fromTo(menuAnimate, { autoAlpha: 0, y: 100 }, { autoAlpha: 1, y: 0, duration: 2, stagger: 0.5 });
+    };
+
+    useEffect(() => {
+        animateMenu();
+    }, []);
     
     return (
     <section className="menu-card-section">
         <div className="menu-title">Browse Our Menu</div>
-            <div className="menu-card-template">
+            <div className="menu-card-template" ref={menuRef}>
             {  menuList.map((menu) => (
                 <div className="menu-card-container">
                     <ul className="menu-cards">
