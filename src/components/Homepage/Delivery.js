812c7578fbf9a59e466a218e364ea1f2ce93c464
devalibello <alibelloimoukhuede@gmail.com>
2024-08-02 20:55:21 +0100

    update offer images setting 3

--- src/components/Homepage/Delivery.js
+++ src/components/Homepage/Delivery.js
@@ -7,29 +7,31 @@ import deliveryIconTwo from '../../assets/delivery-icon-2.webp'
 import deliveryIconThree from '../../assets/delivery-icon-3.webp'
 
 const Delivery = () => (
-    <section className="delivery-section">
-        <div className="picture-left">
-            <img src={deliveryImageLeft} alt="" className="delivery-image-left" />
-        </div>
-        <div className="picture-middle">
-            <img src={deliveryImageUp} alt="" className="delivery-image-up" />
-            <img src={deliveryImageDown} alt="" className="delivery-image-down" />
-        </div>
-        <div className="delivery-section-text">
-            <div className="delivery-title">Fast Food <br /> Delivery in city</div>
-            <div className="delivery-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, voluptate.</div>
-            <div className='delivery-icon-section'>
-                <div className='delivery-icon'>
-                    <img src={deliveryIconOne} alt="" className="delivery-icon-1" />
-                    <div>Delivery within 30 mins</div>
-                </div>
-                <div className='delivery-icon'>
-                    <img src={deliveryIconTwo} alt="" className="delivery-icon-2" />
-                    <div>Best Offer & Prices</div>
-                </div>
-                <div className='delivery-icon'>
-                    <img src={deliveryIconThree} alt="" className="delivery-icon-3" />
-                    <div>Online Service available</div>
+    <section className="delivery-section-background">
+        <div className="delivery-section">
+            <div className="picture-left">
+                <img src={deliveryImageLeft} alt="" className="delivery-image-left" />
+            </div>
+            <div className="picture-middle">
+                <img src={deliveryImageUp} alt="" className="delivery-image-up" />
+                <img src={deliveryImageDown} alt="" className="delivery-image-down" />
+            </div>
+            <div className="delivery-section-text">
+                <div className="delivery-title">Fast Food <br /> Delivery in city</div>
+                <div className="delivery-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, voluptate.</div>
+                <div className='delivery-icon-section'>
+                    <div className='delivery-icon'>
+                        <img src={deliveryIconOne} alt="" className="delivery-icon-1" />
+                        <div>Delivery within 30 mins</div>
+                    </div>
+                    <div className='delivery-icon'>
+                        <img src={deliveryIconTwo} alt="" className="delivery-icon-2" />
+                        <div>Best Offer & Prices</div>
+                    </div>
+                    <div className='delivery-icon'>
+                        <img src={deliveryIconThree} alt="" className="delivery-icon-3" />
+                        <div>Online Service available</div>
+                    </div>
                 </div>
             </div>
         </div>
