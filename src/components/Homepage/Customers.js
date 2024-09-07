2581d3739de768508cd26119f9bac9e388d02e1d
devalibello <alibelloimoukhuede@gmail.com>
2024-09-07 08:58:51 +0100

    Update customer section final 1

--- src/components/Homepage/Customers.js
+++ src/components/Homepage/Customers.js
@@ -13,7 +13,7 @@ const customers = [
     {
         id: 1,
         image: firstCustomerImage,
-        title: ' A Unique and Unforgettable Dining Experience',
+        title: ' An Unforgettable Dining Experience',
         description: "The food here is absolutely amazing! Every dish is crafted to perfection, and the ambiance is just right.",
         name: "Sophie Robbert",
         location: "Los Angeles, CA"
